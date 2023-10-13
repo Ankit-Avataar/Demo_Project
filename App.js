ec5808200ed9c1829de551be187466ed80fdaaac
Ankit-Avataar <ankit.chauhan@avataarskin.com>
2023-10-13 05:44:35 -0700

    Get Text Input Value

--- App.js
+++ App.js
@@ -1,59 +1,28 @@
 import { useState } from 'react';
-import { Button, Text, View, StyleSheet } from 'react-native';
+import { Text, TextInput, View, StyleSheet, Button} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState("");
   return (
-    <View style = {styles.container}>
-      <Text style = {styles.heading}>Avataar</Text>
-      <Text style = {styles.services}>Services</Text>
-      <Text style = {styles.available}>ADVANCED LASER FACIALS</Text>
-      <Text style = {styles.available}>PERMANENT HAIR REDUCTION</Text>
-      <Text style = {styles.available}>SKIN REPAIR</Text>
-      <Text style = {styles.available}>WEIGHT LOSS</Text>
-      <Text style = {styles.available}>DIET PLANS</Text>
-      <Text style = {styles.available}>FACE SURGERIES</Text>
+    <View>
+      <Text style={{fontSize: 30, textAlign: 'center', marginTop: 80}}>Handle text input</Text>
+      <Text style={{fontSize: 30, textAlign: 'center', marginTop: 80}}>Your name is: {name}</Text>
+      <TextInput 
+      style = {styles.textInput}
+      placeholder='Enter Your Name' 
+      value={name}
+      onChangeText={(text)=>setName(text)}/>
+      <Button title='Clear Input Value' onPress={()=>setName('')}/>
     </View>
   );
 }
 
-const User = (props) => {
-  return (
-    <View>
-    <Text>He is a {props.designation} and his name is {props.name}</Text>
-  </View>
-  );
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  heading: {
-    flex: 1,
-    marginTop: 80,
-    color: 'black',
-    fontSize: 80,
-  },
-  services: {
-    flex: 1,
-    color: 'black',
-    fontSize: 40,
-  },
-  available: {
-    flex: 1,
-    color: '#fff',
-    fontSize: 25,
-    backgroundColor: 'blue',
-    marginBottom: 10,
-    padding: 10,
-    borderRadius: 10,
-    height: 100,
-    textAlignVertical: 'center',
-    textAlign: 'center',
-    borderColor: 'blue',
-    borderWidth: 2,
+  textInput: {
+  fontSize: 18, 
+  color: 'black', 
+  borderWidth: 2, 
+  borderColor: 'blue', 
+  margin: 10
   }
-});
+})
