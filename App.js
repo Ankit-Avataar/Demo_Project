4fbfa4a8dc63122911d380fdb59f8cda2c61270c
Ankit-Avataar <ankit.chauhan@avataarskin.com>
2023-10-13 04:55:29 -0700

    Demo Project

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <Text style = {styles.heading}>Avataar</Text>
+      <Text style = {styles.services}>Services</Text>
+      <Text style = {styles.available}>ADVANCED LASER FACIALS</Text>
+      <Text style = {styles.available}>PERMANENT HAIR REDUCTION</Text>
+      <Text style = {styles.available}>SKIN REPAIR</Text>
+      <Text style = {styles.available}>WEIGHT LOSS</Text>
+      <Text style = {styles.available}>DIET PLANS</Text>
+      <Text style = {styles.available}>FACE SURGERIES</Text>
     </View>
+  );
+}
+
+const User = (props) => {
+  return (
+    <View>
+    <Text>He is a {props.designation} and his name is {props.name}</Text>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'white',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  heading: {
+    flex: 1,
+    marginTop: 80,
+    color: 'black',
+    fontSize: 80,
+  },
+  services: {
+    flex: 1,
+    color: 'black',
+    fontSize: 40,
+  },
+  available: {
+    flex: 1,
+    color: '#fff',
+    fontSize: 25,
+    backgroundColor: 'blue',
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 10,
+    height: 100,
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    borderColor: 'red',
+    borderWidth: 2,
+  }
 });
